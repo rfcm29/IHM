b19f51329e34dd56d8e1f234070fc484e689858f
IRPAndrade <IRPAndrade@gmail.com>
2021-05-31 19:48:14 +0100

    erro linha 29 pagamentos_page_ts

--- src/app/pagamento/pagamento.page.ts
+++ src/app/pagamento/pagamento.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NODATA } from 'dns';
 
 @Component({
   selector: 'app-pagamento',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagamentoPage implements OnInit {
 
-  constructor() { }
+  MetodosForm: FormGroup;
+  isSubmitted: boolean;
+
+  constructor(public formBuilder: FormBuilder, private router: Router) {
+
+    this.isSubmitted= false;
+  }
 
   ngOnInit() {
+
+    this.MetodosForm= this.formBuilder.group({
+
+        numCartao: ['', [Validators.required], [Validators.minLength(16)], [Validators.maxLength(16)]],
+        dataExp: ['', [Validators.required], [Validators.pattern('^[0-1]+[1-9]+/[0-9]+[0-9]')]],
+        CVV:['', [Validators.required], [Validators.pattern('^[0-9]+[0-9]+[0-9]')]],
+        nomeProp:['', [[Validators.required]]
+    });
+
+  }
+
+  submitForm(){
+    this.isSubmitted = true;
+    if(!this.MetodosForm.valid){
+      return false;
+    } else{
+      
+    }
+  }
+
+
+  get formControls(){
+    return this.MetodosForm.controls;
   }
 
 }
